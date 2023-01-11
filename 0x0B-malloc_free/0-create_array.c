792a2a8667806eee5e4ed6244b41b35516cc1141
MARY_MENGISTU <mengistumary60@gmail.com>
2023-01-11 03:50:59 -0800

    b malloc T-1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * creat_array - entry point
+ * create_array - entry point
  * @size: unsigned int m.
  * @c: char.
  *
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *s;
 	unsigned int m;
-	s = malloc(sizeof(c) * size);
+s = malloc(sizeof(c) * size);
 	if (size == 0 || s == NULL)
 	{
 		return (NULL);
