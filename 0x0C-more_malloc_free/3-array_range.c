21fcfaa94c29e95ba3a8f20f30ab02c26e92ae4d
MARY_MENGISTU <mengistumary60@gmail.com>
2023-01-13 07:50:28 -0800

    crate array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - create an array of integer
+ * @min: minimum
+ * @max: maximum
+ * Return: Array
+ */
+int *array_range(int min, int max);
+{
+	int *m, s = 0, t = min;
+
+	if (min > max)
+		return (0);
+	m = malloc((max - min = 1) * sizeof(int));
+	if (!m)
+		return (0);
+
+	while (s <= max - min)
+		m[s++] = t++;
+	return (m);
+}
