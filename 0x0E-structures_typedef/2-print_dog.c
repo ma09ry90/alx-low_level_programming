626f6280ec43f144c044409ab0df065d8d5269ec
MARY_MENGISTU <mengistumary60@gmail.com>
2023-01-16 06:41:42 -0800

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	}
 	else
-	{	
+	{
 		printf("Name: %s\n", d->name);
 	}
 		printf("Age: %.6f\n", d->age);
@@ -24,7 +24,7 @@ void print_dog(struct dog *d)
 		printf("Owner: (nil)\n");
 	}
 	else
-	{	
+	{
 		printf("Owner: %s\n", d->owner);
 	}
 }
