d011e535f000aff303097409aeed5145bd054ada
MARY_MENGISTU <mengistumary60@gmail.com>
2023-01-31 06:54:51 -0800

    task-5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,19 +9,13 @@
 
 void free_listint2(listint_t **head)
 {
-listint_t *cursor;
-listint_t **temp = head;
-
-if (temp != NULL)
-{
-while (*head != NULL)
-{
-cursor = *head;
-free(cursor);
-*head = (*head)->next;
-}
-
-*temp = NULL;
-
-}
+	if (!head || !*head)
+		return;
+	while (*head)
+	{
+		listint_t *tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = 0;
 }
