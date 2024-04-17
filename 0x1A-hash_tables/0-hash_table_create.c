e3170685bdad7e754c877fda42fbdb8db92ec2a1
ma09ry90 <mengistumary60@gmail.com>
2024-04-17 05:04:31 -0700

    0x1A-hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,18 +13,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 
-	table = calloc(1,sizeof(hash_table_t));
+	hash_table = malloc(1,sizeof(hash_table_t));
 
-	if (table == NULL)
+	if (hash_table == NULL)
 		return (NULL);
 
-	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
-	if (table->array == NULL)
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(table);
+		free(hash_table);
 		return (NULL);
 	}
-	return (table);
+	return (hash_table);
 
 }
