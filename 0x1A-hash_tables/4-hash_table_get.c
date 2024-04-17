a0ac9dc607f1b124d39e9ddae691e7ba175228d0
ma09ry90 <mengistumary60@gmail.com>
2024-04-17 05:13:54 -0700

    hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,22 +7,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *node;
+	unsigned long int index;
 
-hash_node_t *node;
-unsigned long int index;
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-if (ht == NULL)
-	return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 
-index = key_index((const unsigned char *) key, ht->size);
-node = ht->array[index];
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
 
-while (node != NULL)
-{
-	if (strcmp(node->key, key) == 0)
-		return (node->value);
-
-	node = node->next;
-}
-return (NULL);
+	return ((node == NULL) ? NULL : node->value);
 }
