1d03a904ac25501d339f5352a36d7b2de34fc4f8
ma09ry90 <mengistumary60@gmail.com>
2024-07-16 01:40:11 -0700

    search

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,49 +1,45 @@
 #include "search_algos.h"
 
 /**
- * _advanced_binary - performs advanced binary search
- * @array: the integer array
- * @size: its size
- * @value: value to search for
+ * jump_list - Searches for an algorithm in a sorted singly
+ *             linked list of integers using jump search.
+ * @list: A pointer to the  head of the linked list to search.
+ * @size: The number of nodes in the list.
+ * @value: The value to search for.
  *
- * Return: the index found or -1
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
  */
-int *_advanced_binary(int *array, size_t size, int value)
+listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t i = 0;
+	size_t step, step_size;
+	listint_t *node, *jump;
 
-	if (!size || !array)
+	if (list == NULL || size == 0)
 		return (NULL);
-	for (printf("Searching in array: "); i < size; i++)
-		printf("%d%s", array[i], i + 1 == size ? "\n" : ", ");
 
-	i = (size - 1) / 2;
-	if (array[i] == value)
+	step = 0;
+	step_size = sqrt(size);
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
 	{
-		if (i)
-			return (_advanced_binary(array, i + 1, value));
-		return (array + i);
+		node = jump;
+		for (step += step_size; jump->index < step; jump = jump->next)
+		{
+			if (jump->index + 1 == size)
+				break;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
 	}
-	else if (array[i] > value)
-		return (_advanced_binary(array, i + 1, value));
-	else
-		return (_advanced_binary(array + i + 1, size - i - 1, value));
-}
 
-/**
- * advanced_binary - performs advanced binary search
- * @array: the integer array
- * @size: its size
- * @value: value to search for
- *
- * Return: the index found or -1
- */
-int advanced_binary(int *array, size_t size, int value)
-{
-	int *a = _advanced_binary(array, size, value);
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
 
-	if (!a)
-		return (-1);
-	else
-		return (a - array);
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
 }
